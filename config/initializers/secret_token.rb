20dbef762af89a248b5c595b967b051475ce3aa5
Michael Hurwicz <mhurwicz@gmail.com>
2012-12-31 18:54:20 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rcal2::Application.config.secret_token = '10d7177c9c216c9e0143bf6e2e75a04316d65afc67e6e11f05d48e2ed6140b6336be3ccec8d6352b57b23c28e7afab1e2aa9596fdf7147db5eb8e8d69482dc7b'
